4786dc8889a90ab95e59b7dff695ed0b72a646ca
RAskiNthagreat <kcraskin2@gmail.com>
2026-07-05 22:44:19 +0545

    Initial project setup

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Java Quiz Game ");
+        System.out.println("=================================");
+
+        System.out.println("Project setup successful!");
+    }
+
+}
